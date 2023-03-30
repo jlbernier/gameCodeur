16cc03b45d84a5c0d744f2c313e282c5c704c9f0
jlbernier <sureplouf35@gmail.com>
2023-03-30 13:03:48 +0200

    class with deltatime and spritebatch

---
+++ CSharp/test_monogame/test_monogame/Button.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_monogame
+{
+    public delegate void OnClick(Button pSender);
+    public class Button
+    {
+        public Vector2 Position { get; set; }
+        public Rectangle BoundingBox { get; set; }
+        public bool isHover { get; private set; }
+        private MouseState oldMouseState;
+        public OnClick onClick { get; set; }
+        public Texture2D Texture { get; }
+        public bool ToRemove { get; set; }
+
+
+        public Button(Texture2D pTexture, int x, int y)
+        {
+            Texture = pTexture;
+            ToRemove = false;
+            Position = new Vector2(x, y);
+        }
+
+        public void Update(GameTime pGameTime)
+        {
+            MouseState newMouseState = Mouse.GetState();
+            Point MousePos = newMouseState.Position;
+
+            if (BoundingBox.Contains(MousePos))
+            {
+                if (!isHover)
+                {
+                    isHover = true;
+                    Debug.WriteLine("The button is now hover !");
+                }
+            }
+            else
+            {
+                if (isHover)
+                {
+                    Debug.WriteLine("The button is no more hover !");
+                }
+                isHover = false;
+            }
+            if (isHover)
+            {
+                if ((newMouseState.LeftButton == ButtonState.Pressed) &&
+                    (oldMouseState.LeftButton == ButtonState.Released))
+                {
+                    Debug.WriteLine("Button is click!");
+                    if (onClick != null)
+                    {
+                        onClick(this);
+                    }
+                }
+            }
+            oldMouseState = newMouseState;
+            BoundingBox = new Rectangle(
+              (int)Position.X,
+              (int)Position.Y,
+              Texture.Width,
+              Texture.Height);
+        }
+        public virtual void Draw(SpriteBatch pSpriteBatch)
+        {
+            pSpriteBatch.Draw(Texture, Position, null, Color.White);
+        }
+    }
+}
---
+++ CSharp/test_monogame/test_monogame/MainGame.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace test_monogame
+{
+    public class MainGame : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Button MyButton;
+
+        public MainGame()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            MyButton = new Button(this.Content.Load<Texture2D>("button"), 100, 100);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            MyButton.Update(gameTime);
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            MyButton.Draw(_spriteBatch);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
